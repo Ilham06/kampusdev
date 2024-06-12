92293f877379eb003d8be1bfa588ea05db3691cf
ilhamvorta <ilham@vorta.live>
2024-06-12 11:25:52 +0700

    repository setup

---
+++ app/Repositories/TestimoniRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories;
+
+class TestimoniRepository
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
